d34aac6e22ec7503fe668f7f9c0d615332512ee2
bfloyd14 <brendanflo1425@gmail.com>
2024-04-15 23:01:28 -0400

    add new.ejs view and html.

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,10 +1,20 @@
 import {Flight} from '../models/flight.js'
 
 function index(req, res){
-
+  Flight.find({})
+  .then(flights =>{
+    res.render('flights/index',{
+      flights,
+      title: 'All Flights'
+    })
+  })
+  .catch(err =>{
+    console.log(err)
+    res.redirect('/')
+    })
 }
 
 export {
   index,
-  
+
 }
